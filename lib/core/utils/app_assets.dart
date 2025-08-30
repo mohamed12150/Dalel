8701646f149633bcad597b83806494553955fd6f
Mohamed Al-khair Rabeh <alkhair621@gmail.com>
2025-08-31 02:59:33 +0300

    app color

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImages1
+  /// assets/images/1.png
+  static const String assetsImages1 = "assets/images/1.png";
+
+  /// Assets for assetsImages2
+  /// assets/images/2.png
+  static const String assetsImages2 = "assets/images/2.png";
+
+  /// Assets for assetsImages3
+  /// assets/images/3.png
+  static const String assetsImages3 = "assets/images/3.png";
+
+  /// Assets for assetsImages4
+  /// assets/images/4.svg
+  static const String assetsImages4 = "assets/images/4.svg";
+
+  /// Assets for assetsImages5
+  /// assets/images/5.svg
+  static const String assetsImages5 = "assets/images/5.svg";
+
+  /// Assets for assetsImagesPana
+  /// assets/images/pana.png
+  static const String assetsImagesPana = "assets/images/pana.png";
+}
+
---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0xFFC49E85);
+
+  static const Color primaryVariant = Color(0xFF333333);
+
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color surface = Color(0xFF6F6460);
+  static const Color error = Color(0xFFB00020);
+}
