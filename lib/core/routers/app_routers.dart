af95acec62d7f26415c257e7d37c30866ca4a7a4
Mohamed Al-khair Rabeh <alkhair621@gmail.com>
2025-09-03 01:02:05 +0300

    splash view

--- lib/core/routers/app_routers.dart
+++ lib/core/routers/app_routers.dart
@@ -1,6 +1,13 @@
+import 'package:dalel/features/onborading/presentaion/view/onboarding.dart';
 import 'package:dalel/features/splash/presentation/views/splash.dart';
 import 'package:go_router/go_router.dart' show GoRouter, GoRoute;
 
 final GoRouter router = GoRouter(
-  routes: [GoRoute(path: '/', builder: (context, state) => const SplashView())],
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => const SplashView()),
+    GoRoute(
+      path: '/onboarding',
+      builder: (context, state) => const Onboarding(),
+    ),
+  ],
 );
--- lib/features/splash/presentation/views/splash.dart
+++ lib/features/splash/presentation/views/splash.dart
@@ -1,9 +1,28 @@
+import 'package:dalel/core/funcation/navegation.dart';
 import 'package:dalel/core/utils/app_strings.dart';
 import 'package:dalel/core/utils/app_textstyle.dart';
+
 import 'package:flutter/material.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    delay();
+  }
+
+  void delay() {
+    Future.delayed(const Duration(seconds: 3), () {
+      CustomNavigate(context, '/onboarding');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
